1996986fdc5b47b4dcf5218465ef63a0b31ae879
devGenArkada <dev_gen@arkada-web.ru>
2021-10-12 11:40:48 +0300

    first page ready

--- build/frontend/js/main.js
+++ build/frontend/js/main.js
@@ -282,27 +282,38 @@ var appContent = (function() {
   };
 
   var blocks = function() {
-    var headerHeight = $('.header').height();
-    var firstScreenHeight = 'calc(100vh - ' + headerHeight + 'px)';
-    $('.firstScreen').css('min-height', firstScreenHeight);
-    
-    var firstScreenHeight = $('.firstScreen').height();
-    var firstScreenTitleHeight = $('.firstScreen__title').height();
-    var firstScreenBlockHeight = 'calc(' +firstScreenHeight + 'px - ' + +firstScreenTitleHeight + 'px)';
-    $('.firstScreenBlock').css('min-height', firstScreenBlockHeight);
-    // console.log(firstScreenBlockHeight);
 
+    $( window ).resize(function() {
+      // header Height
+      var headerHeight = $('.header').height();
+      var firstScreenHeight = 'calc(100vh - ' + headerHeight + 'px)';
+      $('.firstScreen').css('min-height', firstScreenHeight);      
+      
+      // firstScreen Height
+      var firstScreenHeight = $('.firstScreen').height();
+      var firstScreenTitleHeight = $('.firstScreen__title').height();
+      var firstScreenBlockHeight = 'calc(' +firstScreenHeight + 'px - ' + +firstScreenTitleHeight + 'px)';
+      $('.firstScreenBlock').css('min-height', firstScreenBlockHeight);
+  
+      // firstScreenBlock width
+      var firstScreenBlockLeftWidth = $('.firstScreenBlockLeft').width();
+      var firstScreenTitleStrongLeft = +firstScreenBlockLeftWidth + 20;
+      var firstScreenTitleStrongLeft = firstScreenTitleStrongLeft + 'px';
+      $('.firstScreen__title strong').css('left', firstScreenTitleStrongLeft);
+      
+      // firstScreenTitle Height
+      var firstScreenTitleHeight = $('.firstScreen__title').height();
+      var firstScreenTitleStrongTop = +firstScreenTitleHeight + 20;
+      var firstScreenTitleStrongTop = firstScreenTitleStrongTop + 'px';
+      $('.firstScreen__title strong').css('top', firstScreenTitleStrongTop);
+      
+      // firstScreenBlockLeftList max-width
+      var firstScreenBlockLeftWidth = $('.firstScreenBlockLeft').width();
+      var firstScreenBlockLeftListMaxWidth = +firstScreenBlockLeftWidth -30;
+      var firstScreenBlockLeftListMaxWidth = +firstScreenBlockLeftListMaxWidth + 'px';
+      $('.firstScreenBlockLeftList').css('max-width', firstScreenBlockLeftListMaxWidth);
+    }).resize();
 
-    // firstScreenBlock width
-    var firstScreenBlockLeftWidth = $('.firstScreenBlockLeft').width();
-    var firstScreenTitleStrongLeft = +firstScreenBlockLeftWidth + 20;
-    var firstScreenTitleStrongLeft = firstScreenTitleStrongLeft + 'px';
-    $('.firstScreen__title strong').css('left', firstScreenTitleStrongLeft);
-
-    var firstScreenTitleHeight = $('.firstScreen__title').height();
-    var firstScreenTitleStrongTop = +firstScreenTitleHeight + 20;
-    var firstScreenTitleStrongTop = firstScreenTitleStrongTop + 'px';
-    $('.firstScreen__title strong').css('top', firstScreenTitleStrongTop);
 
     // var 
     
