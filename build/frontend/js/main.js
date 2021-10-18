53c6bc6542ed12d350f259a6eba03e28bffb0ea9
devGenArkada <dev_gen@arkada-web.ru>
2021-10-18 17:35:55 +0300

    features page adaptive

--- build/frontend/js/main.js
+++ build/frontend/js/main.js
@@ -42,6 +42,38 @@ window.onload = function() {
         var firstScreenBlockLeftListMaxWidth = +firstScreenBlockLeftListMaxWidth + 'px';
         $('.firstScreenBlockLeftList').css('max-width', firstScreenBlockLeftListMaxWidth);
       }
+
+      function featuresBlocks () {
+
+        function thirdRowSecondColumn() {
+          let firstColumnOffset = $('.featuresSectionRowColumn--start .featuresSectionRowItem').offset().top
+          let firstColumnHeight = $('.featuresSectionRowColumn--start .featuresSectionRowItem').height()
+          let secondColumnOffset = $('.featuresSectionRowColumn--center').offset().top
+          let newOffsetSecondColumnTop = (firstColumnOffset + firstColumnHeight)
+          if (secondColumnOffset < newOffsetSecondColumnTop) {
+            let secondColumnTop = newOffsetSecondColumnTop - secondColumnOffset + 'px'
+            $('.featuresSectionRowColumn--center').css('top', secondColumnTop);
+            thirdRowThirdColumn();
+          }
+        }
+
+        function thirdRowThirdColumn () {
+          let secondColumnOffset = $('.featuresSectionRowColumn--center').offset().top
+          let secondColumnHeight = $('.featuresSectionRowColumn--center').height()
+          let thirdColumnOffset = $('.featuresSectionRowColumn--end').offset().top
+          let newOffsetThirdColumnTop = (secondColumnOffset + secondColumnHeight)
+          if (thirdColumnOffset < newOffsetThirdColumnTop) {
+            let thirdColumnOffset = $('.featuresSectionRowColumn--end').offset().top
+            let thirdColumnTop = newOffsetThirdColumnTop - thirdColumnOffset + 30 + 'px'
+            $('.featuresSectionRowColumn--end').css('top', thirdColumnTop);
+          }
+        }
+
+        
+        thirdRowSecondColumn();
+        thirdRowThirdColumn();
+      }
+      
       
       if(window.screen.width >= 960) {
         headerFunction(); 
@@ -50,6 +82,11 @@ window.onload = function() {
         firstScreenBlockLeftWidthFunction();
         firstScreenBlockLeftMaxWidthFunction();
       }
+
+      if(window.screen.width >= 1200) {
+        featuresBlocks();
+      }
+
 
       // headerFunction(); 
       // firstScreenTitleFunction();
