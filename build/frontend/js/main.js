8d675fc5a3856c4f3194b1a8b4fdafe9c8aa9e57
devGenArkada <dev_gen@arkada-web.ru>
2021-10-18 10:58:06 +0300

    js new functions and structure

--- build/frontend/js/main.js
+++ build/frontend/js/main.js
@@ -1,47 +1,50 @@
 'use strict';
 
 window.onload = function() {
-  $(window).on('resize', function(){
-    console.log('fdfdf');    
-  });
 
   var blocks = function() {
     $( window ).resize(function() {
-      console.log('1');
       // header Height
-      var headerHeight = $('.header').height();
-      var firstScreenHeight = 'calc(100vh - ' + headerHeight + 'px)';
-      $('.firstScreen').css('min-height', firstScreenHeight);
+      var headerFunction = function() {
+        var headerHeight = $('.header').height();
+        var firstScreenHeight = 'calc(100vh - ' + headerHeight + 'px)';
+        $('.firstScreen').css('min-height', firstScreenHeight);
+      }
       
-      // // firstScreenTitle Height
-      var firstScreenTitleHeight = $('.firstScreen__title').height();
-      // console.log(firstScreenTitleHeight);
-      var firstScreenTitleStrongTop = +firstScreenTitleHeight + 20;
-      // console.log(firstScreenTitleStrongTop);
-      var firstScreenTitleStrongTop = firstScreenTitleStrongTop + 'px';
-      // console.log(firstScreenTitleStrongTop);
-      $('.firstScreen__title strong').css('top', firstScreenTitleStrongTop);
+      // firstScreenTitle Height
+      var firstScreenTitleFunction = function() {
+        var firstScreenTitleHeight = $('.firstScreen__title').height();
+        var firstScreenTitleStrongTop = +firstScreenTitleHeight + 20;
+        var firstScreenTitleStrongTop = firstScreenTitleStrongTop + 'px';
+        $('.firstScreen__title strong').css('top', firstScreenTitleStrongTop);
+      }
       
-      // // firstScreen Height
-      var firstScreenHeight = $('.firstScreen').height();
-      console.log(firstScreenHeight);
-      var firstScreenTitleHeight = $('.firstScreen__title').height();
-      console.log(firstScreenTitleHeight);
-      var firstScreenBlockHeight = 'calc(' +firstScreenHeight + 'px - ' + +firstScreenTitleHeight + 'px)';
-      console.log(firstScreenBlockHeight);
-      $('.firstScreenBlock').css('min-height', firstScreenBlockHeight);
+      // firstScreen Height
+      var firstScreenHeightFunction = function(){
+        var firstScreenHeight = $('.firstScreen').height();
+        var firstScreenTitleHeight = $('.firstScreen__title').height();
+        var firstScreenBlockHeight = 'calc(' +firstScreenHeight + 'px - ' + +firstScreenTitleHeight + 'px)';
+        $('.firstScreenBlock').css('min-height', firstScreenBlockHeight);
+      }
   
-      // // firstScreenBlock width
+      // firstScreenBlock width
       var firstScreenBlockLeftWidth = $('.firstScreenBlockLeft').width();
       var firstScreenTitleStrongLeft = +firstScreenBlockLeftWidth + 20;
       var firstScreenTitleStrongLeft = firstScreenTitleStrongLeft + 'px';
       $('.firstScreen__title strong').css('left', firstScreenTitleStrongLeft);
       
-      // // firstScreenBlockLeftList max-width
-      var firstScreenBlockLeftWidth = $('.firstScreenBlockLeft').width();
-      var firstScreenBlockLeftListMaxWidth = +firstScreenBlockLeftWidth -30;
-      var firstScreenBlockLeftListMaxWidth = +firstScreenBlockLeftListMaxWidth + 'px';
-      $('.firstScreenBlockLeftList').css('max-width', firstScreenBlockLeftListMaxWidth);
+      // firstScreenBlockLeftList max-width
+      var firstScreenBlockLeftWidthFunction = function (){
+        var firstScreenBlockLeftWidth = $('.firstScreenBlockLeft').width();
+        var firstScreenBlockLeftListMaxWidth = +firstScreenBlockLeftWidth -30;
+        var firstScreenBlockLeftListMaxWidth = +firstScreenBlockLeftListMaxWidth + 'px';
+        $('.firstScreenBlockLeftList').css('max-width', firstScreenBlockLeftListMaxWidth);
+      }
+
+      headerFunction(); 
+      firstScreenTitleFunction();
+      firstScreenHeightFunction();
+      firstScreenBlockLeftWidthFunction();
     }).resize();
   }
 
